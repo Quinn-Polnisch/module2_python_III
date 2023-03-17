604da837af95dbe981db67b21e3768536469d4cb
Quinn Polnisch <quinn@Quinns-MacBook-Pro.local>
2023-03-17 13:04:21 -0400

    python_III

---
+++ assignment3_module.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def square_foot(length, width):
+    sf = length * width
+    print(f"The square foot of the house/room is {sf}.")
+
+def circumference(radius):
+    circ = radius * 2 * pi
+    print(f"The radius of the circle is {circ}.")
